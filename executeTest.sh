8cf28d9b8bde90be2cf1dfaa163a8996e38fe6c0
Yohannis K Telila <joey.kifle@gmail.com>
2022-04-08 12:25:47 +0200

    update execute Test File

--- executeTest.sh
+++ executeTest.sh
@@ -11,7 +11,7 @@ k=$1
 totalRuns=$2
 
 allThreads=(1 2 4 8 16 32 64 128 256)
-inputFiles=("input_20k_s123.txt", "input_50k_s456.txt", "input_100k_s789.txt") 
+inputFiles=("input_20k_s123.txt" "input_50k_s456.txt" "input_100k_s789.txt") 
 
 echo "k-value: $k"
 echo "Total runs: $totalRuns"
